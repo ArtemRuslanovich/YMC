1a4a898b3fa15df3a286ffecd7e706928a0e9058
Artiom Zavatski <zawatski2018@mail.ru>
2022-02-24 19:47:05 +0300

    created a new application, fixed bugs

---
+++ crm/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render
+from .models import Order
+from .forms import OrderForm
+from cms.models import CmsSlider
+
+
+# Create your views here.
+def first_page(request):
+    slider_list = CmsSlider.objects.all()
+
+    form = OrderForm()
+    dict_obj = { 'slider_list': slider_list,
+                 'form': form,
+                 }
+    return render(request, './index.html', dict_obj)
+
+
+def thanks_page(request):
+    if request.POST:
+        name = request.POST['name']
+        phone = request.POST['phone']
+        element = Order(order_name = name, order_phone = phone)
+        element.save()
+        return render(request, './thanks.html', { 'name': name,})
+    else:
+        return render(request, './thanks.html')
